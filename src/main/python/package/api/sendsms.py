ad6844d303136562cae6f3714751a4390fa38119
Thepas <45799484+Thepas@users.noreply.github.com>
2021-06-09 15:37:32 +0200

    Update sendsms.py
    modify account SID and auth token

--- src/main/python/package/api/sendsms.py
+++ src/main/python/package/api/sendsms.py
@@ -3,14 +3,14 @@ from twilio.rest import Client
 from package.api.password import Password
 
 # the following line needs your Twilio Account SID and Auth Token
-client = Client("ACbba09c3524af4229668efec0ed7ea4a3", "6395102521c45483db065cba259a0e99")
+client = Client("account SID", "Auth Token")
 
 # change the "from_" number to your Twilio number and the "to" number
 # to the phone number you signed up for Twilio with, or upgrade your
 # account to send SMS to any phone number
 def sendSms():
     code = Password().get_random_sms_password()
-    if client.messages.create(to="+33781571629",
+    if client.messages.create(to="Tel number",
                            from_="+14124192839",
                            body=f"\n Voici ton code connexion: {code}"):
         print("envoyé")
